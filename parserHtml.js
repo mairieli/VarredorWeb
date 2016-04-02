fe014416a458ceb946e1cddb30a7d36066d94b35
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-04-02 15:57:24 -0300

    Função de Busca Atributo

--- parserHtml.js
+++ parserHtml.js
@@ -16,30 +16,35 @@ var parseResponse = function(response) {
 	});
 	response.on('end', function(chunk) {
 		var dom = makeDom(data);
-		
-		var tag = traversal.getChildren(dom[3]);
-		var tag2 = traversal.getChildren(tag[3]);
-		var tag3 = traversal.getChildren(tag2[1]);
-		var tag4 = traversal.getChildren(tag3[5]);
-		var tag5 = traversal.getChildren(tag4[3]);
-		var tag6 = traversal.getChildren(tag5[1]);
-		var tag7 = traversal.getChildren(tag6[3]);
-		var tag8 = traversal.getChildren(tag7[7]);
-		var tag9 = traversal.getChildren(tag8[1]);
-		var tag10 = traversal.getChildren(tag9[1]);
-		var tag11 = traversal.getChildren(tag10[1]);
-		var tag12 = traversal.getChildren(tag11[1]);
-		var tag13 = traversal.getChildren(tag12[1]);
-		var tag14 = traversal.getChildren(tag13[1]);
-		var tag15 = traversal.getChildren(tag14[1]);
-		var tag16 = traversal.getChildren(tag15[1]);
-		var tag17 = traversal.getChildren(tag16[5]);
 
-		var linkNoticia1 = traversal.getAttributeValue(tag17[1], "href");
-		console.log(linkNoticia1);
+		var lista = [];
+		busca(dom, lista, "class", "tileItem visualIEFloatFix")
 
-		var tag18 = traversal.getChildren(tag16[7]);
-		var linkNoticia2 = traversal.getAttributeValue(tag18[1], "href");
-		console.log(linkNoticia2);
+		function logArray(element, index, array){
+			console.log(element)
+			console.log("---------------------")
+		}
+
+		lista.forEach(logArray);
+
+		var lista2 = [];
+		busca(lista[5], lista2, "class", "tileHeadline")
+
+		lista2.forEach(logArray);
+		//console.log(x)
+			
 	});
+}
+
+var busca = function(dom, lista, tipo, atributo){
+	if(dom != null){
+		for(var i = 0; i < dom.length; i++){
+			if(traversal.getAttributeValue(dom[i], tipo) == atributo){
+				lista.push(traversal.getChildren(dom[i]));
+			} else {
+				busca(traversal.getChildren(dom[i]), lista, tipo, atributo);
+			}
+		}
+	}
+	return null;
 }
