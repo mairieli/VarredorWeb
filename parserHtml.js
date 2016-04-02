26c4c12b58b3d7ecdfa6e0e1c16ae201d3316bb8
Mairieli Wessel <mairieliw@gmail.com>
2016-04-02 18:14:30 -0300

    Função título dos links

--- parserHtml.js
+++ parserHtml.js
@@ -33,6 +33,33 @@ var parseResponse = function(response) {
 		var rodape = traversal.getChildren(elemento[1])
 		console.log("Endereço completo do Campus: ")
 		console.log(rodape[0].data)
+
+		var listalnkNoticias = [];
+		busca(dom, listalnkNoticias, "class", "tileHeadline")
+		var element1 = listalnkNoticias[0]
+		var link1 = traversal.getAttributeValue(element1[1], "href")
+		http.get(link1, function(response) {
+			getNoticiaInformcoes(response)
+		}).on('error', function(e) {
+			console.log("Got error: " + e.message);
+		});
+
+		var element2 = listalnkNoticias[1]
+		var link2 = traversal.getAttributeValue(element2[1], "href")
+
+
+		var element3 = listalnkNoticias[4]
+		var link3 = traversal.getAttributeValue(element3[1], "href")
+
+
+		var element4 = listalnkNoticias[5]
+		var link4 = traversal.getAttributeValue(element4[1], "href")
+
+
+		var element5 = listalnkNoticias[6]
+		var link5 = traversal.getAttributeValue(element5[1], "href")
+
+
 	});
 }
 
@@ -47,6 +74,24 @@ var busca = function(dom, lista, tipo, atributo) {
 		}
 	}
 	return null;
+}
+
+var getNoticiaInformcoes = function(response) {
+	var data = "";
+	response.on('data', function(chunk) {
+		data += chunk;
+	});
+	response.on('end', function(chunk) {
+		var dom = makeDom(data);
+		var retorno = [];
+		busca(dom, retorno, "id", "parent-fieldname-title")
+		var elemento = retorno[0]
+		//var textoApresentacao = traversal.getChildren(elemento[3])
+		console.log("\nTítulo: ")
+		console.log(elemento[0].data)
+		console.log("Texto: ")
+
+	});
 }
 
 var getTextoApresentacao = function(response) {
@@ -66,7 +111,8 @@ var getTextoApresentacao = function(response) {
 }
 
 function imprimeTextoApresentacao(element, index, array) {
-	var filhote = traversal.getChildren(element[1])
+	var filho = traversal.getChildren(element[1])
+	var texto = filho[0]
 	console.log("Endereço completo do Campus: ")
-	console.log(filhote[0].data)
+	console.log(texto.data)
 }
