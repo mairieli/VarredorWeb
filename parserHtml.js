b59f090a827670120ebe777841e9f6eae7e6d15e
Bruno Mendes <brunosouza@alunos.utfpr.edu.br>
2016-04-03 02:09:34 -0300

    Finalizado Parse

--- parserHtml.js
+++ parserHtml.js
@@ -31,8 +31,9 @@ var main = function(response) {
 		buscaDoms(dom, listaRodape, "id", "portal-footer")
 		var elemento = listaRodape[0]
 		var rodape = traversal.getChildren(elemento[1])
-		console.log("********Endereço completo do Campus********")
+		console.log("======Endereço completo do Campus======")
 		console.log(rodape[0].data)
+		console.log("_______________________________________________________________________________")
 
 		var listalnkNoticias = [];
 		buscaDoms(dom, listalnkNoticias, "class", "tileHeadline")
@@ -40,7 +41,7 @@ var main = function(response) {
 		var element1 = listalnkNoticias[0]
 		var link1 = traversal.getAttributeValue(element1[1], "href")
 		http.get(link1, function(response) {
-			getNoticiaInformacoes(response)
+			getNoticiaInformacoes(response, true, link1)
 		}).on('error', function(e) {
 			console.log("Got error: " + e.message);
 		});
@@ -48,7 +49,7 @@ var main = function(response) {
 		var element2 = listalnkNoticias[1]
 		var link2 = traversal.getAttributeValue(element2[1], "href")
 		http.get(link2, function(response) {
-			getNoticiaInformacoes(response)
+			getNoticiaInformacoes(response, true, link2)
 		}).on('error', function(e) {
 			console.log("Got error: " + e.message);
 		});
@@ -56,7 +57,7 @@ var main = function(response) {
 		var element3 = listalnkNoticias[4]
 		var link3 = traversal.getAttributeValue(element3[1], "href")
 		http.get(link3, function(response) {
-			getNoticiaInformacoes(response)
+			getNoticiaInformacoes(response, false, link3)
 		}).on('error', function(e) {
 			console.log("Got error: " + e.message);
 		});
@@ -64,7 +65,7 @@ var main = function(response) {
 		var element4 = listalnkNoticias[5]
 		var link4 = traversal.getAttributeValue(element4[1], "href")
 		http.get(link4, function(response) {
-			getNoticiaInformacoes(response)
+			getNoticiaInformacoes(response, false, link4)
 		}).on('error', function(e) {
 			console.log("Got error: " + e.message);
 		});
@@ -73,7 +74,7 @@ var main = function(response) {
 		var element5 = listalnkNoticias[6]
 		var link5 = traversal.getAttributeValue(element5[1], "href")
 		http.get(link5, function(response) {
-			getNoticiaInformacoes(response)
+			getNoticiaInformacoes(response, false, link5)
 		}).on('error', function(e) {
 			console.log("Got error: " + e.message);
 		});
@@ -95,7 +96,7 @@ var buscaDoms = function(dom, lista, tipo, atributo) {
 	return null;
 }
 
-var getNoticiaInformacoes = function(response) {
+var getNoticiaInformacoes = function(response, ehNoticia, link) {
 	var data = "";
 	response.on('data', function(chunk) {
 		data += chunk;
@@ -105,7 +106,7 @@ var getNoticiaInformacoes = function(response) {
 		var titulo = [];
 		buscaDoms(dom, titulo, "id", "parent-fieldname-title")
 		var elemento = titulo[0]
-		console.log("\n********Título********")
+		console.log("\n======Título======")
 
 		// Expressão regular - Regex e Replace
 		// \s - qualquer espaço em branco
@@ -114,18 +115,44 @@ var getNoticiaInformacoes = function(response) {
 		// Substitui por ' '
 		console.log(elemento[0].data.replace(/\s{2,}/g, ' '))
 
+		if (!ehNoticia) {
+			console.log("\n======Link======")
+			console.log(link)
+		}
 
 		var texto = [];
 		buscaDoms(dom, texto, "id", "parent-fieldname-text")
-		var elementoTexto = texto[0]
-		console.log("\n********Texto********")
+
 		var listaTexto = []
-		buscaTextoNoticaiInformacoes(elementoTexto, listaTexto) 
+		buscaTextoNoticaiInformacoes(texto[0], listaTexto)
 		var textoFinal = ""
-		for (valor in listaTexto){
-			textoFinal += listaTexto[valor]
+
+		if(ehNoticia){
+			var domTituloNoticiaNoTexto = []
+			var tituloNoticiaNoTexto = []
+			buscaDoms(dom, domTituloNoticiaNoTexto, "id", "parent-fieldname-description")
+			buscaTextoNoticaiInformacoes(domTituloNoticiaNoTexto[0], tituloNoticiaNoTexto)
+			textoFinal += tituloNoticiaNoTexto[0].replace(/\s{2,}/g, ' ')
 		}
+
+		for (valor in listaTexto) {
+			if (ehNoticia) {
+				if (valor != listaTexto.length - 2) {
+					textoFinal += listaTexto[valor]
+				}
+			} else {
+				textoFinal += listaTexto[valor]
+			}
+		}
+
+		if (ehNoticia) {
+			console.log("\n======Data======")
+			console.log(listaTexto[listaTexto.length - 2])
+		}
+
+		console.log("\n======Texto======")
 		console.log(textoFinal.replace(/\s{2,}/g, '\n'))
+		console.log("_______________________________________________________________________________")
 	});
 }
 
@@ -153,8 +180,9 @@ var getTextoApresentacao = function(response) {
 		buscaDoms(dom, retorno, "id", "parent-fieldname-text-84bff7d47dcef80d890fe2eb7c8d20bb")
 		var elemento = retorno[0]
 		var textoApresentacao = traversal.getChildren(elemento[3])
-		console.log("\n********Texto de Apresentacao********")
+		console.log("\n======Texto de Apresentacao======")
 		console.log(textoApresentacao[0].data)
+		console.log("_______________________________________________________________________________")
 	});
 }
 
