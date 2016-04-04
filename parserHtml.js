b25052b6e408549dfd825e65678b4a63b06158ea
Mairieli Wessel <mairieliw@gmail.com>
2016-04-03 23:01:18 -0300

    modularização

--- parserHtml.js
+++ parserHtml.js
@@ -1,13 +1,6 @@
-var makeDom = require("./makeDom").makeDom;
+var treeDom = require("./treeDom");
 var http = require('http');
-var traversal = require('domutils');
-
-var url = 'http://www.utfpr.edu.br/campomourao';
-http.get(url, function(response) {
-	main(response);
-}).on('error', function(e) {
-	console.log("Got error: " + e.message);
-});
+var domutils = require('domutils');
 
 var main = function(response) {
 	var data = "";
@@ -15,85 +8,77 @@ var main = function(response) {
 		data += chunk;
 	});
 	response.on('end', function(chunk) {
-		var dom = makeDom(data);
+		var dom = treeDom.getTreeDom(data);
 
-		var listaApresentacao = [];
-		buscaDoms(dom, listaApresentacao, "class", "navTreeItem visualNoMarker navTreeFolderish section-o-campus");
-		var element = listaApresentacao[0];
-		var linkOCampus = traversal.getAttributeValue(element[1], "href");
-		http.get(linkOCampus, function(response) {
-			getTextoApresentacao(response);
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
+		imprimeTextoApresentacao(dom);
 
-		var listaRodape = [];
-		buscaDoms(dom, listaRodape, "id", "portal-footer");
-		var elemento = listaRodape[0];
-		var rodape = traversal.getChildren(elemento[1]);
-		console.log("======Endereço completo do Campus======");
-		console.log(rodape[0].data);
-		console.log("_______________________________________________________________________________");
+		imprimeRodape(dom);
 
-		var listalnkNoticias = [];
-		buscaDoms(dom, listalnkNoticias, "class", "tileHeadline");
-
-		var element1 = listalnkNoticias[0];
-		var link1 = traversal.getAttributeValue(element1[1], "href");
-		http.get(link1, function(response) {
-			getNoticiaInformacoes(response, true, link1);
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
-
-		var element2 = listalnkNoticias[1];
-		var link2 = traversal.getAttributeValue(element2[1], "href");
-		http.get(link2, function(response) {
-			getNoticiaInformacoes(response, true, link2);
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
-
-		var element3 = listalnkNoticias[4];
-		var link3 = traversal.getAttributeValue(element3[1], "href");
-		http.get(link3, function(response) {
-			getNoticiaInformacoes(response, false, link3);
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
-
-		var element4 = listalnkNoticias[5];
-		var link4 = traversal.getAttributeValue(element4[1], "href");
-		http.get(link4, function(response) {
-			getNoticiaInformacoes(response, false, link4);
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
-
-
-		var element5 = listalnkNoticias[6];
-		var link5 = traversal.getAttributeValue(element5[1], "href");
-		http.get(link5, function(response) {
-			getNoticiaInformacoes(response, false, link5);
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
-
-
+		imprimeNoticiasInformacoes(dom);
 	});
 };
 
-var buscaDoms = function(dom, lista, tipo, atributo) {
-	if (dom) {
-		for (var i = 0; i < dom.length; i++) {
-			if (traversal.getAttributeValue(dom[i], tipo) == atributo) {
-				lista.push(traversal.getChildren(dom[i]));
-			} else {
-				buscaDoms(traversal.getChildren(dom[i]), lista, tipo, atributo);
-			}
+var imprimeTextoApresentacao = function(dom) {
+	var listaApresentacao = [];
+	treeDom.getNodesDom(dom, listaApresentacao, "class", "navTreeItem visualNoMarker navTreeFolderish section-o-campus");
+	var element = listaApresentacao[0];
+	var linkOCampus = domutils.getAttributeValue(element[1], "href");
+	http.get(linkOCampus, function(response) {
+		getTextoApresentacao(response);
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+};
+
+var imprimeRodape = function(dom) {
+	var listaRodape = [];
+	treeDom.getNodesDom(dom, listaRodape, "id", "portal-footer");
+	var elemento = listaRodape[0];
+	var rodape = domutils.getChildren(elemento[1]);
+	console.log("======Endereço completo do Campus======");
+	console.log(rodape[0].data);
+	console.log("_______________________________________________________________________________");
+};
+
+var imprimeNoticiasInformacoes = function(dom) {
+	var listalnkNoticias = [];
+	treeDom.getNodesDom(dom, listalnkNoticias, "class", "tileHeadline");
+	listalnkNoticias.splice(2, 2);
+	listalnkNoticias.splice(5, 3);
+	for (var i = 0; i < listalnkNoticias.length; i++) {
+		var elemento = listalnkNoticias[i];
+		if (i > 1) {
+			imprimeInformacoesInstitucionais(elemento);
+		} else {
+			imprimeNoticia(elemento);
 		}
 	}
-	return null;
+};
+
+var imprimeNoticia = function(elemento) {
+	var link = domutils.getAttributeValue(elemento[1], "href");
+	http.get(link, function(response) {
+		getNoticiaInformacoes(response, true, link);
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+
+};
+
+var imprimeInformacoesInstitucionais = function(elemento) {
+	var link = domutils.getAttributeValue(elemento[1], "href");
+	http.get(link, function(response) {
+		getNoticiaInformacoes(response, false, link);
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+};
+
+function imprimeTextoApresentacao(element, index, array) {
+	var filho = domutils.getChildren(element[1]);
+	var texto = filho[0];
+	console.log("Endereço completo do Campus: ");
+	console.log(texto.data);
 };
 
 var getNoticiaInformacoes = function(response, ehNoticia, link) {
@@ -102,9 +87,9 @@ var getNoticiaInformacoes = function(response, ehNoticia, link) {
 		data += chunk;
 	});
 	response.on('end', function(chunk) {
-		var dom = makeDom(data);
+		var dom = treeDom.getTreeDom(data);
 		var titulo = [];
-		buscaDoms(dom, titulo, "id", "parent-fieldname-title");
+		treeDom.getNodesDom(dom, titulo, "id", "parent-fieldname-title");
 		var elemento = titulo[0];
 		console.log("\n======Título======");
 
@@ -121,17 +106,17 @@ var getNoticiaInformacoes = function(response, ehNoticia, link) {
 		}
 
 		var texto = [];
-		buscaDoms(dom, texto, "id", "parent-fieldname-text");
+		treeDom.getNodesDom(dom, texto, "id", "parent-fieldname-text");
 
 		var listaTexto = [];
-		buscaTextoNoticaiInformacoes(texto[0], listaTexto);
+		buscaTextoNoticiaInformacoes(texto[0], listaTexto);
 		var textoFinal = "";
 
-		if(ehNoticia){
+		if (ehNoticia) {
 			var domTituloNoticiaNoTexto = [];
 			var tituloNoticiaNoTexto = [];
-			buscaDoms(dom, domTituloNoticiaNoTexto, "id", "parent-fieldname-description");
-			buscaTextoNoticaiInformacoes(domTituloNoticiaNoTexto[0], tituloNoticiaNoTexto);
+			treeDom.getNodesDom(dom, domTituloNoticiaNoTexto, "id", "parent-fieldname-description");
+			buscaTextoNoticiaInformacoes(domTituloNoticiaNoTexto[0], tituloNoticiaNoTexto);
 			textoFinal += tituloNoticiaNoTexto[0].replace(/\s{2,}/g, ' ');
 		}
 
@@ -156,13 +141,13 @@ var getNoticiaInformacoes = function(response, ehNoticia, link) {
 	});
 };
 
-var buscaTextoNoticaiInformacoes = function(dom, listaTexto) {
+var buscaTextoNoticiaInformacoes = function(dom, listaTexto) {
 	if (dom) {
 		for (var i = 0; i < dom.length; i++) {
 			if (dom[i].data) {
 				listaTexto.push(dom[i].data);
 			} else {
-				buscaTextoNoticaiInformacoes(traversal.getChildren(dom[i]), listaTexto);
+				buscaTextoNoticiaInformacoes(domutils.getChildren(dom[i]), listaTexto);
 			}
 		}
 	}
@@ -175,20 +160,21 @@ var getTextoApresentacao = function(response) {
 		data += chunk;
 	});
 	response.on('end', function(chunk) {
-		var dom = makeDom(data);
+		var dom = treeDom.getTreeDom(data);
 		var retorno = [];
-		buscaDoms(dom, retorno, "id", "parent-fieldname-text-84bff7d47dcef80d890fe2eb7c8d20bb");
+		treeDom.getNodesDom(dom, retorno, "id", "parent-fieldname-text-84bff7d47dcef80d890fe2eb7c8d20bb");
 		var elemento = retorno[0];
-		var textoApresentacao = traversal.getChildren(elemento[3]);
+		var textoApresentacao = domutils.getChildren(elemento[3]);
 		console.log("\n======Texto de Apresentacao======");
 		console.log(textoApresentacao[0].data);
 		console.log("_______________________________________________________________________________");
 	});
 };
 
-function imprimeTextoApresentacao(element, index, array) {
-	var filho = traversal.getChildren(element[1]);
-	var texto = filho[0];
-	console.log("Endereço completo do Campus: ");
-	console.log(texto.data);
-}
+exports.parserHtml = function(url) {
+	http.get(url, function(response) {
+		main(response);
+	}).on('error', function(e) {
+		console.log("Got error: " + e.message);
+	});
+};
